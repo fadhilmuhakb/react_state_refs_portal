379201eedc2a2f7b2344eb32018c154b6fdb2af4
fadhilmuhakb <fadhilmuhakb@gmail.com>
2024-04-01 14:42:38 +0700

    update: handling project creation & updating the UI

--- src/App.jsx
+++ src/App.jsx
@@ -20,15 +20,31 @@ function App() {
     });
   }
 
+  function handleAddProject(projectData) {
+    setProjectState(prevState => {
+      const projectId = Math.random();
+      const newProject = {
+        ...projectData,
+        id: projectId,
+      };
+
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject]
+      };
+    })
+  }
+  console.log(projectState);
   let content;
   if(projectState.selectedProjectId === null) {
-    content = <NewProject />
+    content = <NewProject onAdd={handleAddProject} />
   } else if(projectState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
   }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectSidebar onStartAddProject={handleStartAddProject} projects={projectState.projects} />
       {content}
     </main>
   );
